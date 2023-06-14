43813f9d669f81816daae0408c683ec253462a87
vikassuresh4397 <vikassuresh4397@gmail.com>
2023-06-14 22:57:48 +0530

    payment page, calculator

---
+++ frontend/src/Pages/Payment.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CreditCard from './Payment-Components/CreditCard'
+
+const Payment = () => {
+  return (
+    <div>
+<CreditCard>
+  
+</CreditCard>
+        
+
+    </div>
+  )
+}
+
+
+
+export default Payment
