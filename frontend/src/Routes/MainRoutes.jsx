af836a625a19cb187bbf7a162917ed5c1a0bde05
Surajy001 <soorajy001@gmail.com>
2023-06-14 22:54:45 +0530

    Signin and signup Frontend

---
+++ frontend/src/Routes/MainRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Signin } from "../Pages/Signin";
+import { Signup } from "../Pages/Signup";
+import Home from "../Pages/Home";
+
+export const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+};
