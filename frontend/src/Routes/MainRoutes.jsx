f3ffbbd7114dfe967f0357b0856f14c146a818f1
Kunal Deotale <kdeotale4@gmail.com>
2023-06-17 01:36:05 +0530

    add from and admin side curd operation

--- frontend/src/Routes/MainRoutes.jsx
+++ frontend/src/Routes/MainRoutes.jsx
@@ -3,7 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import { Signin } from "../Pages/Signin";
 import { Signup } from "../Pages/Signup";
 import Home from "../Pages/Home";
-import {Buyproduct} from "../Pages/BuyProduct/Buyproduct"
+import Property from "../admin/propertyList/Property";
+import EditProperty from "../admin/propertyList/EditProperty";
+import AddProperty from "../admin/add property/AddProperty";
 
 export const MainRoutes = () => {
   return (
@@ -11,7 +13,10 @@ export const MainRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/buyproduct" element={<Buyproduct />} />
+      <Route path="/adminProperty" element={<Property />} />
+      <Route path="/edit/:id" element={<EditProperty />} />
+      <Route path="/addProperty" element={<AddProperty />} />
+      {/* <Route path="/buyproduct" element={<Buyproduct />} /> */}
     </Routes>
   );
 };
