f22e3a6642356b568170193cb3b4ff57a8170317
Kunal Deotale <kdeotale4@gmail.com>
2023-06-17 01:35:43 +0530

    add from and admin side curd operation

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,8 +1,10 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
-import {reducer as BuyReducer} from "./BuyReducer/reducer"
-const rootReducer = combineReducers({ 
-    BuyReducer
- })
+import { reducer as BuyReducer } from "./BuyReducer/reducer";
+import { adminPropertyReducer } from "./admin/adminProperty/adminPropertyReducer";
+const rootReducer = combineReducers({
+  BuyReducer,
+  adminPropertyReducer,
+});
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
