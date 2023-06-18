b74d639c5b3a47e637b5eb41a00b07286f00cebd
Surajy001 <soorajy001@gmail.com>
2023-06-19 03:50:13 +0530

    Homepage works

--- frontend/src/Components/Home/TopPartOfHomePage/TopPartOfHomePage.jsx
+++ frontend/src/Components/Home/TopPartOfHomePage/TopPartOfHomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TopPartOfHomePage.css';
-
+import { IconButton, Input, Stack, useColorModeValue } from '@chakra-ui/react';
+import { BiSearch } from 'react-icons/bi';
 const TopPartOfHomePage = () => {
   return (
     <div id="TopPartOfHomePage" className="centered">
@@ -19,13 +20,39 @@ const TopPartOfHomePage = () => {
     </div>
       
 
-      <div class="search-bar">
+      {/* <div class="search-bar">
   <input type="text" class="search-input" placeholder="Search..." />
  
-   <button id="boom1">search🔎</button>
- 
-</div>
+   <button id="boom1">search🔎</button>   
+</div> */}
 
+<Stack direction={'row'} gap={0} marginTop={"10px"}>
+              <Input
+                padding={"24px 0px"}
+                placeholder={'Search...'}
+                bg={useColorModeValue('white.500', 'white.700')}
+                border={0}
+                _focus={{
+                  bg: 'white.300',
+                }}
+                width={{base:"200px" ,sm: "180px", md: "250px",lg:"300px" }}
+                borderTopRightRadius={"none"}
+                borderBottomRightRadius={"none"}
+              />
+              <IconButton
+                bg={useColorModeValue('red.400', 'red.800')}
+                color={useColorModeValue('white', 'white.800')}
+                _hover={{
+                  bg: 'red.600',
+                }}
+                padding={"24px 0px"}
+                width={{base:"60px" ,sm: "50px", md: "60px",lg:"70px" }}
+                borderTopLeftRadius={"none"}
+                borderBottomLeftRadius={"none"}
+                aria-label="Subscribe"
+                icon={<BiSearch />}
+              />
+            </Stack>
       </div>
      
     </div>
